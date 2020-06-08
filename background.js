92e6041bdd55ca9f209c20a14e2572f87143dc59
mi2 <51777985+3ts75@users.noreply.github.com>
2020-06-08 12:42:13 +0900

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  Particles.init({
-    selector: '.background',
-    sizeVariations: 30,
-    color: [
-      '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
-    ]
-  });
-};
